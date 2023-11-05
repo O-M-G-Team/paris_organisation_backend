c72ca49904240cf17fba7cc60953ee4a23cb00bc
PloyyNK <nartnatta_kri@yahoo.com>
2023-11-06 00:10:43 +0700

    add api link

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from model import ParisDB
 import requests
 
-#mongodb driver
+# mongodb driver
 import motor.motor_asyncio
 
 client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
@@ -11,40 +11,46 @@ database = client.ParisDB
 collection = database.sport_info
 
 # fetch from IOC and store in Paris database
-async def fetch_IOC(api_url):
-    res = requests.get(api_url)
-    data = res.json()
-    
-    sport_id = data.get("sport_id")
-    sport_name = data.get("sport_name")
-    participating_country = data.get("participating_country")
-    datetime = data.get("datetime")
-    result = data.get("result")
-    
-    existing_sport_info = await collection.find_one({"sport_id": sport_id})
-    
-    if existing_sport_info:
-        await update_sport_info(
-            sport_id, sport_name, participating_country, datetime, result
-        )
-        return "Sport info updated"
-    else:
-        new_sport_info = {
-            "sport_id": sport_id,
-            "sport_name": sport_name,
-            "participating_country": participating_country,
-            "date_time": datetime,
-            "result": result,
-        }
-        
-        inserted_document = await create_sport_info(new_sport_info)
-        return "Sport info created"
+async def fetch_IOC():
+    try:
+        res = requests.get("https://nongnop.azurewebsites.net/match_table")
+        res.raise_for_status()
+        data = res.json()
+
+        for event in data:
+            sport_id = event["_id"]
+            sport_name = event["sport_name"]
+            participating_country = event["participating_country"]
+            date_time = event["datetime"]
+            result = event["result"]
+
+            existing_sport_info = await collection.find_one({"sport_id": sport_id})
+
+            if existing_sport_info:
+                await update_sport_info(
+                    sport_id, sport_name, participating_country, date_time, result
+                )
+            else:
+                new_sport_info = {
+                    "sport_id": sport_id,
+                    "sport_name": sport_name,
+                    "participating_country": participating_country,
+                    "date_time": date_time,
+                    "result": result,
+                }
+
+                inserted_document = await create_sport_info(new_sport_info)
+
+        return "Sport info updated or created successfully"
+    except requests.RequestException as e:
+        return f"Failed to fetch data from the API: {str(e)}"
 
 
-######################Example CRUD request######################
+###################### Example CRUD request######################
 async def fetch_one_sport_info(sport_id):
     document = await collection.find_one({"sport_id": sport_id})
     return document
+
 
 async def fetch_all_sport_infos():
     sport_infos = []
@@ -52,29 +58,32 @@ async def fetch_all_sport_infos():
     async for document in cursor:
         sport_infos.append(ParisDB(**document))
     return sport_infos
-    
+
+
 async def create_sport_info(sport_info):
     document = sport_info
     # find existing sport_id
     try:
         temp = await collection.find_one({"sport_id": sport_info["sport_id"]})
-        if temp["sport_id"] ==  sport_info["sport_id"]:
+        if temp["sport_id"] == sport_info["sport_id"]:
             return False
     except:
         result = await collection.insert_one(document)
         return document
 
+
 async def update_sport_info(sport_id, sport_name, participating_country, date_time, result):
-    
+
     await collection.update_one({"sport_id": sport_id}, {"$set": {
         "sport_id": sport_id,
         "sport_name": sport_name,
-        "participating_country":participating_country,
+        "participating_country": participating_country,
         "date_time": date_time,
         "result": result
     }})
     document = await collection.find_one({"sport_id": sport_id})
     return document
+
 
 async def remove_sport_info(sport_id):
     await collection.delete_one({"sport_id": sport_id})
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from database import (
+    fetch_one_sport_info,
+    fetch_all_sport_infos,
+    create_sport_info,
+    update_sport_info,
+    remove_sport_info,
+    fetch_IOC
+)
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from model import ParisDB
@@ -7,41 +15,44 @@ from datetime import datetime
 # App object
 app = FastAPI()
 
-from database import (
-    fetch_one_sport_info,
-    fetch_all_sport_infos,
-    create_sport_info,
-    update_sport_info,
-    remove_sport_info
-)
-
 origins = ['http://localhost:5173']
 
 app.add_middleware(
-    CORSMiddleware, 
-    allow_origins = origins,
-    allow_credentials = True,
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
 
 @app.get("/")
 def read_root():
     return {"paris": "organisation"}
 
-######################Example CRUD request######################
+###################### Example CRUD request######################
+
 
 @app.get("/paris_org/olympic/sport_info")
 async def get_sport_info():
     response = await fetch_all_sport_infos()
     return response
 
+
+@app.post("/insert_data_IOC")
+async def insert_data_IOC():
+    result = await fetch_IOC()
+    return result
+
+
 @app.get("/api/sport_info/{sport_id}", response_model=ParisDB)
 async def get_sport_info_by_sport_id(sport_id):
     response = await fetch_one_sport_info(sport_id)
     if response:
         return response
-    raise HTTPException(404, f"there is no sport_info item with this sport_id {sport_id}")
+    raise HTTPException(
+        404, f"there is no sport_info item with this sport_id {sport_id}")
+
 
 @app.post("/api/sport_info", response_model=ParisDB)
 async def post_sport_info(sport_info: ParisDB):
@@ -52,19 +63,23 @@ async def post_sport_info(sport_info: ParisDB):
 
 
 @app.put("/api/sport_info/{sport_id}", response_model=ParisDB)
-async def put_sport_info(sport_id:str, sport_name:str,  participating_country: List[str], date_time: datetime, result: List[Dict]):
+async def put_sport_info(sport_id: str, sport_name: str,  participating_country: List[str], date_time: datetime, result: List[Dict]):
     response = await update_sport_info(sport_id, sport_name, participating_country, date_time, result)
     if response:
         return response
-    raise HTTPException(404, f"there is no sport_info item with this sport_id {sport_id}")
+    raise HTTPException(
+        404, f"there is no sport_info item with this sport_id {sport_id}")
+
 
 @app.delete("/api/sport_info/{sport_id}")
 async def delete_sport_info(sport_id):
     response = await remove_sport_info(sport_id)
     if response:
         return "Succesfully deleted sport_info item"
-    raise HTTPException(404, f"there is no sport_info item with this sport_id {sport_id}")
+    raise HTTPException(
+        404, f"there is no sport_info item with this sport_id {sport_id}")
 ##################################################################
+
 
 @app.get("/paris_org/olympic/{sport_id}", response_model=ParisDB)
 async def get_sport_detail_by_sport_id(sport_id):
