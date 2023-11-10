ae420a9606e674d1d64f6b0df6e679591d74ae57
pinpimarn <pinpimarn.p@ku.th>
2023-11-10 20:40:37 +0700

    Remove result model from import

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from model import ParisDB, Result
+from model import ParisDB
 from typing import List, Dict
 from datetime import datetime
 
