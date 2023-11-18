0265d3ad9af0e5778934effdda941d6b0eddee70
PloyyNK <nartnatta_kri@yahoo.com>
2023-11-18 21:29:52 +0700

    recheck sport result

--- database.py
+++ database.py
@@ -14,10 +14,12 @@ collection = database.sport_info
 
 # fetch from IOC and store in Paris database
 
+
 async def fetch_api():
 
     try:
-        res = requests.get("https://nongnop.azurewebsites.net/match_table/Final")
+        res = requests.get(
+            "https://nongnop.azurewebsites.net/match_table/round/Final")
         res.raise_for_status()
         data = res.json()
 
@@ -27,22 +29,23 @@ async def fetch_api():
             sport_type = event["sport_type"]
             participating_country = event["participating_country"]
             date_time = parser.parse(event["datetime"])
-            result = {}
 
             existing_sport_info = await collection.find_one({"sport_id": sport_id})
 
             if existing_sport_info:
-                await update_sport_info(
-                    sport_id, sport_name, participating_country, date_time, sport_type
-                )
+                info = await fetch_one_sport_info(sport_id)
+                if info['result']:
+                    await update_sport_info(
+                        sport_id, sport_name, participating_country, date_time, info['result'], sport_type
+                    )
             else:
                 new_sport_info = {
                     "sport_id": sport_id,
                     "sport_name": sport_name,
                     "sport_type": sport_type,
                     "participating_country": participating_country,
-                    "date_time": date_time, 
-                    "result": result
+                    "date_time": date_time,
+                    "result": {}
                 }
 
                 doc = await create_sport_info(new_sport_info)
@@ -52,10 +55,9 @@ async def fetch_api():
         return f"Failed to fetch data from the API: {str(e)}"
 
 
-
 ###################### Example CRUD request######################
 async def update_sport_result(sport_id, result):
-    document = await collection.find_one_and_update({"sport_id": sport_id},{'$set': {"result": result}})
+    document = await collection.find_one_and_update({"sport_id": sport_id}, {'$set': {"result": result}})
     return document
 
 
